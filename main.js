396e40ca1d4afd772f956d7432e116dc25c63ad8
HarveyThePooka404 <sylv1612@stud.kea.dk>
2020-02-07 11:08:44 +0100

    copy-pasted -- pre-fucked up

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-fetch("getBanas.php")
+fetch("https://kea-alt-del.dk/t5/api/productlist    ")
   .then(e => e.json())
   .then(buildIt);
 
 function buildIt(data) {
   console.log(data);
 }
+
+// this is fetching the data from the Google sheet //
+
